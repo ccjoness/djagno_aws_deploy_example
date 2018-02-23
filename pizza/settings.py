4d44a0a89610bfb8eff8d3c05432f25e74bdd876
Chris <chris.charles.jones@gmail.com>
2018-02-23 11:56:23 -0800

    trying to deploy with correct pip

--- pizza/settings.py
+++ pizza/settings.py
@@ -118,3 +118,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'pages.User'
+
+try:
+    from pizza.local_settings import *
+except:
+    pass
